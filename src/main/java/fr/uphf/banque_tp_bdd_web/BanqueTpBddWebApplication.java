162bd55c7a30715ce70ba86c9a98ccb18e4bbe90
mathcrinn <mathis.crinchon@epsi.fr>
2023-11-21 17:10:14 +0100

    Debut

---
+++ src/main/java/fr/uphf/banque_tp_bdd_web/BanqueTpBddWebApplication.java
@@ -0,0 +1,13 @@
+package fr.uphf.banque_tp_bdd_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanqueTpBddWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanqueTpBddWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/uphf/banque_tp_bdd_web/BanqueTpBddWebApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.uphf.banque_tp_bdd_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanqueTpBddWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
